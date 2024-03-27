ee7a2f94cd2034f3665299160991986ea01ce7c7
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-03-28 00:15:05 +0900

    Add score calculation script using MS-SSIM metric

---
+++ .score-calculation/calculate.py
@@ -0,0 +1,9 @@
+from skimage.metrics import structural_similarity as ssim
+import cv2
+
+expect = cv2.imread('../question/img/except.png.png')
+answer = cv2.imread('../app/build/outputs/roborazzi/com.github.nidroid.uiperfectcopycontesttest.ExampleUnitTest.yourAnswerTest.png')
+
+msssim_value = ssim(expect, answer, multichannel=True)
+
+print(f'MS-SSIM: {msssim_value}')
