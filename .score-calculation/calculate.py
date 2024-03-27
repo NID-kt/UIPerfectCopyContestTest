8f79b104713b74883dd1d949d956f045dd6ccf69
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-03-28 00:40:32 +0900

    Update image file paths in score calculation script

--- .score-calculation/calculate.py
+++ .score-calculation/calculate.py
@@ -1,8 +1,8 @@
 from skimage.metrics import structural_similarity as ssim
 import cv2
 
-expect = cv2.imread('../question/img/except.png.png')
-answer = cv2.imread('../app/build/outputs/roborazzi/com.github.nidroid.uiperfectcopycontesttest.ExampleUnitTest.yourAnswerTest.png')
+expect = cv2.imread('./question/img/except.png.png')
+answer = cv2.imread('./app/build/outputs/roborazzi/com.github.nidroid.uiperfectcopycontesttest.ExampleUnitTest.yourAnswerTest.png')
 
 msssim_value = ssim(expect, answer, multichannel=True)
 
