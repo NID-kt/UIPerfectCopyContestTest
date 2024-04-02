d0f343dde11140fe8a9303e3e8a9356443211c72
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-04-03 01:31:19 +0900

    Add image upload functionality

--- score-calculation/calculate.py
+++ score-calculation/calculate.py
@@ -1,7 +1,17 @@
 from skimage.metrics import structural_similarity as ssim
 import cv2
+from vercel_storage import blob
+import os
 
 file = './app/build/outputs/roborazzi/com.github.nidroid.uiperfectcopycontesttest.ExampleUnitTest.yourAnswerTest.png'
+actor = os.getenv('GITHUB_ACTOR')
+run_number = os.getenv('GITHUB_RUN_NUMBER')
+
+with open(file, 'rb') as fp:
+    blob.put(
+        pathname='api/v1/blob/' + actor + '/' + run_number + '.png',
+        body=fp.read()
+    )
 
 expect = cv2.imread('./question/img/except.png')
 answer = cv2.imread(file)
